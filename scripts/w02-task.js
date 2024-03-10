4b592d533a19545cddc022ea3d13b8cf3bd23879
rag5798 <bgoettman@yahoo.com>
2024-03-10 00:32:42 -0700

    week 2

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -4,20 +4,20 @@
 
 /* Step 2 - Variables */
 let fullName = 'Robert Goettman';
-let cuurentYear = new Date().getFullYear();
+let currentYear = new Date().getFullYear(); // Corrected typo
 let profilePicture = 'images/me_2.jpg';
 
 /* Step 3 - Element Variables */
 const nameElement = document.getElementById('name');
 const foodElement = document.getElementById('food');
-const yearElement = document.querySelector('#year')
+const yearElement = document.querySelector('#year');
 const imageElement = document.getElementById('image');
 
 /* Step 4 - Adding Content */
-nameElement.innerHTML = `<strong>${fullName}</strong>`
+nameElement.innerHTML = `<strong>${fullName}</strong>`;
 yearElement.textContent = currentYear;
 imageElement.setAttribute('src', profilePicture);
-imageElement.setAttribute('alt', `Profile image of ${fullName}`)
+imageElement.setAttribute('alt', `Profile image of ${fullName}`);
 
 /* Step 5 - Array */
 let favoriteFoods = [];
@@ -29,8 +29,3 @@ favoriteFoods.shift();
 foodElement.textContent += "<br>" + favoriteFoods.join("<br>");
 favoriteFoods.pop();
 foodElement.textContent += "<br>" + favoriteFoods.join("<br>");
-
-
-
-
-
