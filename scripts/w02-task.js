603974cb4b14f4cbe63dcb23096a44719605ac27
rag5798 <bgoettman@yahoo.com>
2024-03-10 00:27:54 -0700

    week 2

--- scripts/w02-task.js
+++ scripts/w02-task.js
@@ -3,25 +3,32 @@
 /* Step 1 - Setup type tasks - no code required */
 
 /* Step 2 - Variables */
-
-
-
+let fullName = 'Robert Goettman';
+let cuurentYear = new Date().getFullYear();
+let profilePicture = 'images/me_2.jpg';
 
 /* Step 3 - Element Variables */
-
-
-
-
+const nameElement = document.getElementById('name');
+const foodElement = document.getElementById('food');
+const yearElement = document.querySelector('#year')
+const imageElement = document.getElementById('image');
 
 /* Step 4 - Adding Content */
-
-
-
-
-
+nameElement.innerHTML = `<strong>${fullName}</strong>`
+yearElement.textContent = currentYear;
+imageElement.setAttribute('src', profilePicture);
+imageElement.setAttribute('alt', `Profile image of ${fullName}`)
 
 /* Step 5 - Array */
-
+let favoriteFoods = [];
+foodElement.textContent = favoriteFoods;
+var newFavoriteFood = "Sushi";
+favoriteFoods.push(newFavoriteFood);
+foodElement.textContent += "<br>" + newFavoriteFood;
+favoriteFoods.shift();
+foodElement.textContent += "<br>" + favoriteFoods.join("<br>");
+favoriteFoods.pop();
+foodElement.textContent += "<br>" + favoriteFoods.join("<br>");
 
 
 
