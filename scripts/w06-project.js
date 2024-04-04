d7a1bf9f10891c6383f27b0a321ce7792c56b25a
rag5798 <bgoettman@yahoo.com>
2024-04-04 09:02:05 -0600

    imageUrl

--- scripts/w06-project.js
+++ scripts/w06-project.js
@@ -9,21 +9,24 @@ const displayRecipes = async (recipes) => {
         let img = document.createElement('img');
         img.setAttribute('src', recipe.ImageUrl);
         img.setAttribute('alt', recipe.Name);
-        let list = document.createElement('li')
-        list.textContent = recipe.IngredientList;
+        let ingredientList = document.createElement('li')
+        ingredientList.textContent = recipe.IngredientList;
+        let instructionList = document.createElement('li');
+        instructionList.textContent = recipe.InstructionList;
         article.appendChild(img);
         article.appendChild(h3);
-        article.appendChild(list);
+        article.appendChild(ingredientList);
+        article.appendChild(instructionList);
         templesElement.appendChild(article);
     });
 }
 
 const getRecipes = async () => {
-    const response = await fetch('');
+    const response = await fetch('https://raw.githubusercontent.com/rag5798/cse121b/main/data/recipes.json');
     const data = await response.json();
     recipeList = data;
-    console.log(templeList);
-    displayRecipes(templeList);
+    console.log(recipeList);
+    displayRecipes(recipeList);
 }
 
 getRecipes();
